6dc103a28fce6c51d27ccfdfd720abad70f9713e
Misha Kaharlykskyi <mishakagar@gmail.com>
2017-04-10 20:45:06 +0300

    heroku cleardb settings added

--- config/db_params.php
+++ config/db_params.php
@@ -1,7 +1,12 @@
 <?php
 return array (
-    'host' => '127.0.0.1',
+    /*'host' => '127.0.0.1',
     'dbname' => 'sport_db',
     'user' => 'root',
-    'password' => 'root'
+    'password' => 'root'*/
+    'host' => 'eu-cdbr-west-01.cleardb.com',
+    'dbname' => 'heroku_fa9666f2d88bafb',
+    'user' => 'b5e19923d98b4b',
+    'password' => '32c2f5e2'
+
 );
